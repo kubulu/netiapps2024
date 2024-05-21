c9bcc9e14063c0ad2b6a97f83e50b4b1e7087e95
shashi <shashi09568@gmail.com>
2024-05-21 16:44:30 +0530

    ui Changes

--- components/casestudySection/casestudySection.tsx
+++ components/casestudySection/casestudySection.tsx
@@ -1,14 +1,28 @@
 import React from "react";
-import styles from "./casestudySection.module.scss"
+import Slider from "react-slick";
+import styles from "./casestudySection.module.scss";
 
 
 export default function CasestudySection() {
-    return(
-        <div className={styles.casestudy}>
-            <h2 className={`headingFont text-center`}>Take a look at our projects</h2>
-            <div className={styles.casestudyBlock}>
-                <div className={`row justify-content-center align-items-center`}>
-                    <div className={`col-6`}>
+    const settings = {
+        className: "center",
+        centerMode: true,
+        infinite: true,
+        centerPadding: "340px",
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 2000,
+        autoplaySpeed: 4000,
+        autoplay: true,
+        pauseOnHover: true,
+    };
+    return (
+        <div className={`casestudySection`}>
+            <div className={styles.casestudy}>
+                <h2 className={`headingFont text-center`}>Take a look at our projects</h2>
+                <Slider {...settings}>
+                    <div className={styles.casestudyBlock}>
+
                         <div className={styles.casestudyCard}>
                             <div className={styles.casestudyContent}>
                                 <h4>Document Lifecycle and Workflow Management</h4>
@@ -20,11 +34,113 @@ export default function CasestudySection() {
                                 </ul>
                             </div>
                             <div className={styles.casestudyImg}>
-                                <img src={`/images/casestudy.png`} />
+                                <img src={`/images/casestudy.png`}/>
                             </div>
                         </div>
                     </div>
-                </div>
+                    <div className={styles.casestudyBlock}>
+
+                        <div className={styles.casestudyCard}>
+                            <div className={styles.casestudyContent}>
+                                <h4>Document Lifecycle and Workflow Management</h4>
+                                <ul>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                </ul>
+                            </div>
+                            <div className={styles.casestudyImg}>
+                                <img src={`/images/casestudy.png`}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.casestudyBlock}>
+
+                        <div className={styles.casestudyCard}>
+                            <div className={styles.casestudyContent}>
+                                <h4>Document Lifecycle and Workflow Management</h4>
+                                <ul>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                </ul>
+                            </div>
+                            <div className={styles.casestudyImg}>
+                                <img src={`/images/casestudy.png`}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.casestudyBlock}>
+
+                        <div className={styles.casestudyCard}>
+                            <div className={styles.casestudyContent}>
+                                <h4>Document Lifecycle and Workflow Management</h4>
+                                <ul>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                </ul>
+                            </div>
+                            <div className={styles.casestudyImg}>
+                                <img src={`/images/casestudy.png`}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.casestudyBlock}>
+
+                        <div className={styles.casestudyCard}>
+                            <div className={styles.casestudyContent}>
+                                <h4>Document Lifecycle and Workflow Management</h4>
+                                <ul>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                </ul>
+                            </div>
+                            <div className={styles.casestudyImg}>
+                                <img src={`/images/casestudy.png`}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.casestudyBlock}>
+
+                        <div className={styles.casestudyCard}>
+                            <div className={styles.casestudyContent}>
+                                <h4>Document Lifecycle and Workflow Management</h4>
+                                <ul>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                </ul>
+                            </div>
+                            <div className={styles.casestudyImg}>
+                                <img src={`/images/casestudy.png`}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.casestudyBlock}>
+
+                        <div className={styles.casestudyCard}>
+                            <div className={styles.casestudyContent}>
+                                <h4>Document Lifecycle and Workflow Management</h4>
+                                <ul>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                    <li>Web Developement</li>
+                                </ul>
+                            </div>
+                            <div className={styles.casestudyImg}>
+                                <img src={`/images/casestudy.png`}/>
+                            </div>
+                        </div>
+                    </div>
+                </Slider>
             </div>
         </div>
     )
--- components/testimonialsSection/testimonialsSection.tsx
+++ components/testimonialsSection/testimonialsSection.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import Slider from "react-slick";
 import styles from "./testimonialsSection.module.scss"
 
 
 export default function TestimonialsSection() {
+    const settings = {
+        dots: true,
+        fade: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        autoplay:true,
+        slidesToScroll: 1,
+        waitForAnimate: false,
+        arrows: false,
+        pauseOnHover: false
+    };
     return(
         <div className={styles.testimonials}>
             <div className={'container'}>
@@ -18,10 +31,27 @@ export default function TestimonialsSection() {
                                 <div className={styles.quote}>
                                     <img src={`/images/mark.svg`} />
                                 </div>
+                                <Slider {...settings}>
                                 <div className={styles.testimonialContent}>
                                     <p>Altran has been working with NetiApps for a number of years now and we are very pleased with the dedication and responsiveness of the NetiApps team. NetiApps is a much valued vendor/partner, who go out of their way to meet deadlines and provide solutions. The NetiApps culture of ingenuity, positive attitude and commitment to the task at hand is much appreciated. We look forward to a long and fruitful relationship.</p>
                                     <h5>Altran Team (A Part of Capgemini Engineering)</h5>
                                 </div>
+
+                                    <div className={styles.testimonialContent}>
+                                        <p>It was great working with NetiApps. Anyone can provide technology solutions but really understanding the business needs of a customer requires dedicated efforts as shown by NetiApps. Their solutions contributed to some outstanding support to automate DTDC’s critical processes.</p>
+                                        <h5>Altran Team (A Part of Capgemini Engineering)</h5>
+                                    </div>
+
+                                    <div className={styles.testimonialContent}>
+                                        <p>Working with NetiApps was a pleasure. They were enthusiastic, quick with suggestions and very reactive with modifications. As a client I felt very comfortable that the work would get done within the determined planning. I highly recommend them and look forward to working with them again.</p>
+                                        <h5>Altran Team (A Part of Capgemini Engineering)</h5>
+                                    </div>
+
+                                    <div className={styles.testimonialContent}>
+                                        <p>Altran has been working with NetiApps for a number of years now and we are very pleased with the dedication and responsiveness of the NetiApps team. NetiApps is a much valued vendor/partner, who go out of their way to meet deadlines and provide solutions. The NetiApps culture of ingenuity, positive attitude and commitment to the task at hand is much appreciated. We look forward to a long and fruitful relationship.</p>
+                                        <h5>Altran Team (A Part of Capgemini Engineering)</h5>
+                                    </div>
+                                </Slider>
                             </div>
                         </div>
                     </div>
