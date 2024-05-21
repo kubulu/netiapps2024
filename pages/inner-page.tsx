d178eb69f85b6dda32d8fac67713f92d3e922e1b
Ragavi14 <ragavi@netiapps.com>
2024-05-21 19:04:26 +0530

    changes

--- pages/inner-page.tsx
+++ pages/inner-page.tsx
@@ -1,4 +1,4 @@
-import "/styles/global.scss";
+// import "/styles/global.scss";
 
 import InsightSection from "../components/insightSection/insightSection";
 import HireSection from "../components/hireSection/hireSection";
